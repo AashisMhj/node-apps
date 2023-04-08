05b74dab3237bedc782658f8f41238d42ad11093
Aashis Maharjan <aashismhj5511@gmail.com>
2023-04-08 17:50:31 +0545

    added api to make traction

--- book-store/app.js
+++ book-store/app.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 const keys = require('./config/key');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const exphbs = require('express-handlebars');
+const cors = require('cors');
 
 let rawData = fs.readFileSync('./datas/index.json');
 let data = JSON.parse(rawData);
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors());
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.json());
@@ -48,6 +50,37 @@ app.get('/api/books/:id', (req, res) =>{
     return res.status(200).json({
         data: book
     })
+})
+
+app.post('/api/charge', async(req, res)=>{
+    const {product_id} = req.body;
+    const product = data.data.find(item => item.id == product_id);
+    if(!product){
+        return res.status(400).json({
+            msg: 'No Book with given ID'
+        });
+    }
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: product.title
+                    },
+                    unit_amount: product.price
+                },
+                quantity: 1
+            }
+        ],
+        mode: 'payment',
+        success_url: `${process.env.FRONT_END_URL}/success`,
+        cancel_url: `${process.env.FRONT_END_URL}/`
+    });
+    return res.status(200).json({
+        id: session.id
+    });
 })
 //
 app.post('/charge', (req, res) =>{
